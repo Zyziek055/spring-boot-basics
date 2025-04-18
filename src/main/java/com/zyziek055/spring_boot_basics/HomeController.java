964887005301e0ec2533c5adf329512041c4687c
Aleksander Mróz <sylgermroz@gmail.com>
2025-04-18 16:40:45 +0200

    @Value test

--- src/main/java/com/zyziek055/spring_boot_basics/HomeController.java
+++ src/main/java/com/zyziek055/spring_boot_basics/HomeController.java
@@ -1,17 +1,16 @@
 package com.zyziek055.spring_boot_basics;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+    @Value("${spring.application.name}")
+    private String appName;
+
     @RequestMapping("/")
     public String index() {
-        String viewName = getViewName();
-        return viewName;
-    }
-
-    private String getViewName() {
-        return "index.html";
-    }
+        System.out.println("App name: " + appName);
+        return "index.html";}
 }
