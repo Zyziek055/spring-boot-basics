35d1d6570682ecb0182d38b0f404af72754c4ec4
Zyziek055 <sylgermroz@gmail.com>
2025-04-23 11:12:54 +0200

    Add Lombok and create User entity

--- src/main/java/com/zyziek055/spring_boot_basics/Store.java
+++ src/main/java/com/zyziek055/spring_boot_basics/Store.java
@@ -1,5 +1,6 @@
 package com.zyziek055.spring_boot_basics;
 
+import com.zyziek055.spring_boot_basics.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -8,7 +9,9 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class Store {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(Store.class, args);
+		//ConfigurableApplicationContext context = SpringApplication.run(Store.class, args);
+		var user = User.builder().name("John").email("<EMAIL>").password("<PASSWORD>").build();
+
 	}
 
 }
---
+++ src/main/java/com/zyziek055/spring_boot_basics/entities/User.java
@@ -0,0 +1,28 @@
+package com.zyziek055.spring_boot_basics.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "users")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, name = "name")
+    private String name;
+
+    @Column(nullable = false, name = "email")
+    private String email;
+
+    @Column(nullable = false, name = "password")
+    private String password;
+
+}
+
